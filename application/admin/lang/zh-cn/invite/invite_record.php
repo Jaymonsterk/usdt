6752b7367bcfb1b102911bc5590ab592a9cc71b9
EricZhang <jaymonsterk@gmail.com>
2021-09-10 02:46:24 +0800

    u

--- application/admin/lang/zh-cn/invite/invite_record.php
+++ application/admin/lang/zh-cn/invite/invite_record.php
@@ -3,7 +3,7 @@
 return [
     'User_id'       => '用户uid',
     'Username'      => '用户名',
-    'Reg_num'       => '当天邀请数',
+    'Reg_num'       => '当天3级团队邀请数',
     'Recharge_usdt' => '充U数',
     'Withdraw_rmb'  => '提人民币数',
     'Dates'         => '日期',
